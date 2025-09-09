a80b6f7f3ff30e6d6b3defd7e8a9c566ceae1f92
Elvis Kiprono <elvis19t87@gmail.com>
2025-09-09 11:44:03 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,15 @@
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, onAuthStateChanged, User } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3giyELU6K8NTgILioG4PA4HLGfXtkwFg",
-  authDomain: "chainflow-ai.firebaseapp.com",
-  projectId: "chainflow-ai",
-  storageBucket: "chainflow-ai.firebasestorage.app",
-  messagingSenderId: "193038732906",
-  appId: "1:193038732906:web:c52be3703517673305c413",
+  "projectId": "chainflow-ai",
+  "appId": "1:193038732906:web:c52be3703517673305c413",
+  "storageBucket": "chainflow-ai.firebasestorage.app",
+  "apiKey": "AIzaSyC3giyELU6K8NTgILioG4PA4HLGfXtkwFg",
+  "authDomain": "chainflow-ai.firebaseapp.com",
+  "messagingSenderId": "193038732906"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
